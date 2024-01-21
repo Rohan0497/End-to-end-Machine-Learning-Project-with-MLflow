167517b4f3da51ee8b3bc9ad3e6295a39833bde9
Rohan0497 <78637250+Rohan0497@users.noreply.github.com>
2024-01-21 21:02:09 +0000

    requirements added

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-end-Machine-Learning-Project-with-MLflow"
+AUTHOR_USER_NAME = "Rohan0497"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = ""
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
